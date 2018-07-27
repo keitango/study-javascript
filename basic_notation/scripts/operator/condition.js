5b3dfdfd45275a6c9b0c56b85e0c3fcbb7f7ae9a
keitango <godspeed4246@gmail.com>
2018-07-27 23:04:05 +0900

    add condition.js/html

---
+++ basic_notation/scripts/operator/condition.js
@@ -0,0 +1,7 @@
+// 条件演算子
+let ds_falsy = '';
+let ds_undefined;
+ds_falsy = (ds_falsy === undefined) ? '💃' : ds_falsy;
+ds_undefined = (ds_undefined === undefined) ? '💃' : ds_undefined;
+console.log("止まるんじゃねぇぞ・・・" + ds_falsy);
+console.log("止まるんじゃねぇぞ・・・" + ds_undefined);
