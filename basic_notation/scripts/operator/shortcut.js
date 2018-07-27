6623453767f34649ac1a879d9c0974a31e7b1682
keitango <godspeed4246@gmail.com>
2018-07-27 23:10:49 +0900

    add shortcut.js/html

---
+++ basic_notation/scripts/operator/shortcut.js
@@ -0,0 +1,8 @@
+// ショートカット演算
+let enemy = '';
+enemy = '' || '🍅';
+console.log(''||'🍅');  // 🍅
+console.log("お前ごときが" + enemy + "に勝てると思うな");
+console.log(1||3);  // 1
+console.log(0||3);  // 3
+console.log(false||1);  // 1
