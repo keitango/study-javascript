6c87c171f2ae7f98dcd2b52b59a2de9eef7bf5e1
keitango <godspeed4246@gmail.com>
2018-07-27 22:56:59 +0900

    add destructuring_assignment_obj.html/js

---
+++ basic_notation/scripts/operator/destructuring_assignment_obj.js
@@ -0,0 +1,32 @@
+// オブジェクトの分割
+var vrains = {
+  title : "遊○王Vrains",
+  broadcaster : "テレ●東京",
+  production : "ぎ△ろっぷ"
+}
+var {broadcaster, title, rating_avg = "1.0%"} = vrains;  // 変数の並びはプロパティと異なってもいい，分解しないプロパティがあってもいい
+console.log(broadcaster);
+console.log(title);
+console.log(rating_avg);
+
+// プロパティと異なる変数名に割り当て分割する
+let {title : name, production : studio} = vrains;  // 変数の並びはプロパティと異なってもいい，分解しないプロパティがあってもいい
+console.log(name);
+console.log(studio);
+
+// 入れ子オブジェクトの分解
+let arcv = {
+  title : "遊戯★Arc-V",
+  broadcaster : "💃レビ東京",
+  production : "ぎゃ◇っぷ",
+  character :
+    {
+      hero : "🍅 < れでぃ〜すえ〜んじぇんとるめ〜ん",
+      heroine : "yz",
+      garbage : "ys < yurusu"
+    }
+  }
+let { character : {hero, heroine}, character : {garbage} } = arcv;
+console.log(hero);
+console.log(heroine);
+console.log(garbage);
