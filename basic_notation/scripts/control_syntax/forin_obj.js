587506638456bd5fc3a6ed5b0059e3f8d15c7946
keitango <godspeed4246@gmail.com>
2018-07-28 15:36:43 +0900

    add forin_obj.js

---
+++ basic_notation/scripts/control_syntax/forin_obj.js
@@ -0,0 +1,7 @@
+// for in : オブジェクト
+
+console.log("\nfor in : オブジェクト");
+let shinchan = {name : "nohara shinnosuke", age : 5, favorite : "young woman"}
+for(let key in shinchan){
+    console.log(key + " : " + shinchan[key]);
+}
