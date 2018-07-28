16382f4b173e94093e80ead765f7af0649d272d0
keitango <godspeed4246@gmail.com>
2018-07-29 01:09:03 +0900

    modify forin_ary_ng.js

--- basic_notation/scripts/control_syntax/forin_ary_ng.js
+++ basic_notation/scripts/control_syntax/forin_ary_ng.js
@@ -1,6 +1,3 @@
-// for in : 配列における拡張機能の列挙
-
-console.log("\nfor in : 配列における拡張機能の列挙");
 let noharake = ["shinnosuke", "hirosshi", "misae", "himawari", "shiro"];
 // 配列オブジェクトに拡張機能を追加
 Array.prototype.hoge = function(){};
