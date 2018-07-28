ca4c8d09e88fe7368cb8b88b43db4d3bbed42db6
keitango <godspeed4246@gmail.com>
2018-07-28 15:39:10 +0900

    add forof_ary.html

--- basic_notation/scripts/control_syntax/forin_obj.js
+++ basic_notation/scripts/control_syntax/forin_obj.js
@@ -1,6 +1,3 @@
-// for in : オブジェクト
-
-console.log("\nfor in : オブジェクト");
 let shinchan = {name : "nohara shinnosuke", age : 5, favorite : "young woman"}
 for(let key in shinchan){
     console.log(key + " : " + shinchan[key]);
