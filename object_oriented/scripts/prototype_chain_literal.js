c0fe23b0f16b5bbd3a825aa2072296425839dbf3
keitango <godspeed4246@gmail.com>
2018-08-07 18:05:20 +0900

    add prototype_chain_literal.js

---
+++ object_oriented/scripts/prototype_chain_literal.js
@@ -0,0 +1,50 @@
+// Personクラスを定義
+var Person = function(name, age){
+  this.name = name;
+  this.age = age;
+};
+
+// プロトタイプにメソッドを定義（リテラル）
+Person.prototype = {
+  getName : function(){
+    return this.name;
+  },
+  getAge : function(){
+    return this.age;
+  },
+  introduce : function(){
+    console.log("私は" + this.getName() + ", " + this.getAge() + "歳です.");
+  }
+};
+
+// Playerクラスを定義
+var Player = function(name, age, sport){
+  Person.call(this, name, age);
+  this.sport = sport;
+};
+Player.prototype = new Person();  // サブクラスのプロトタイプに，スーパークラスのインスタンスをセット
+
+// プロトタイプにメソッドを定義（リテラル）-> error
+//Player.prototype = {
+//  getSport : function(){
+//    return this.sport;
+//  },
+//  introduce : function(){  // オーバーロード
+//    console.log("私は" + this.getName() + ", " + this.getAge() + "歳で" + this.getSport() + "の選手です.");
+//  }
+//};
+
+// プロトタイプメソッドを定義
+Player.prototype.getSport = function(){
+  return this.sport;
+};
+Player.prototype.introduce = function(){  // オーバーロード
+  console.log("私は" + this.getName() + ", " + this.getAge() + "歳で" + this.getSport() + "の選手です.");
+};
+
+
+// インスタンス生成
+var takashi = new Person("takashi", 22);
+var kojiro = new Player("kojiro", 45, "baseball");
+takashi.introduce();  // 私はtakashi, 22際です.
+kojiro.introduce();  // 私はkojiro, 45歳でbaseballの選手です.
