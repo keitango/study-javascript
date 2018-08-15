dc8043419dc56f6d533e8d656e22bc72655c6906
keitango <godspeed4246@gmail.com>
2018-08-15 23:12:30 +0900

    set variables to property of object. variable names are  same to property of object.

--- object_oriented/scripts/obj_literal_improvement.js
+++ object_oriented/scripts/obj_literal_improvement.js
@@ -1,11 +1,16 @@
 // オブジェクトリテラルでメソッドを定義
 let member = {
-  name : 'uehara',
+  name : 'uehira',
   birth : new Date(1975,3,3),  // 月は0からはじまる
   toString(){
     return 'name : ' + this.name + ' / birth : ' + this.birth.toLocaleDateString();
   }
 };
-
 console.log(member.birth);
 console.log(member.toString());
+
+// 変数を同盟のプロパティに割り当てる
+let name = 'kojiro';
+let birth = new Date(1973,9,22);
+let member2 = {name, birth};
+console.log(member2);
